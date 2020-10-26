831d3e3e5f77a1789f61295b8fc14fb6d400d2a9
nathakrit061103jnt <57357978+nathakrit061103jnt@users.noreply.github.com>
2020-10-26 20:59:34 +0700

    Update tbl_products.sql

--- tbl_products.sql
+++ tbl_products.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 26, 2020 at 01:08 PM
+-- Generation Time: Oct 26, 2020 at 02:58 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
@@ -20,6 +20,49 @@ SET time_zone = "+00:00";
 --
 -- Database: `php_crud_sweetalert2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `a_username` varchar(100) NOT NULL COMMENT 'อีเมลแอดมิน',
+  `a_password` varchar(200) NOT NULL COMMENT 'รหัสผ่านแอดมิน'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`a_username`, `a_password`) VALUES
+('admin', 'admin@123'),
+('jamesTaTo', 'james@123'),
+('nathakritTato', '12345678');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_customers`
+--
+
+CREATE TABLE `tbl_customers` (
+  `c_id` int(11) NOT NULL COMMENT 'ไอดีลูกค้า',
+  `c_name` varchar(100) NOT NULL COMMENT 'ชื่อลูกค้า',
+  `c_tel` varchar(10) NOT NULL COMMENT 'เบอร์โทรลูกค้า',
+  `c_address` varchar(255) NOT NULL COMMENT 'ที่อยู่ลูกค้า',
+  `c_username` varchar(100) NOT NULL COMMENT 'ชื่อผู้ใช้การเข้าสู่ระบบลูกค้า',
+  `c_password` varchar(255) NOT NULL COMMENT 'รหัสการเข้าสู่ระบบลูกค้า'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_customers`
+--
+
+INSERT INTO `tbl_customers` (`c_id`, `c_name`, `c_tel`, `c_address`, `c_username`, `c_password`) VALUES
+(1, 'พี่เจมส์ ตาโต', '0x0x0x0x0x', '...... UdonThaNi', 'jamesTaTo25', 'pw@12345678'),
+(2, 'Nathakrit Thitsatian', '061*******', '...... UdonThaNi', 'nathakrit@james', 'pw@12345678');
 
 -- --------------------------------------------------------
 
@@ -47,6 +90,18 @@ INSERT INTO `tbl_products` (`p_id`, `p_name`, `p_price`, `p_count`) VALUES
 --
 
 --
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`a_username`);
+
+--
+-- Indexes for table `tbl_customers`
+--
+ALTER TABLE `tbl_customers`
+  ADD PRIMARY KEY (`c_id`);
+
+--
 -- Indexes for table `tbl_products`
 --
 ALTER TABLE `tbl_products`
@@ -55,6 +110,12 @@ ALTER TABLE `tbl_products`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `tbl_customers`
+--
+ALTER TABLE `tbl_customers`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ไอดีลูกค้า', AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_products`
