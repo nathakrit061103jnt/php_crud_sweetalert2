b9dd0f140343f42ead0f49edcb8be3e224d60ae2
std60010912540@acc.msu.ac.th <std60010912540@acc.msu.ac.th>
2020-10-30 23:02:31 +0700

    u

--- php_crud_sweetalert2.sql
+++ php_crud_sweetalert2.sql
@@ -7,9 +7,11 @@
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone
+= "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -27,16 +29,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_admin`
 --
 
-CREATE TABLE `tbl_admin` (
-  `a_username` varchar(100) NOT NULL COMMENT 'อีเมลแอดมิน',
-  `a_password` varchar(200) NOT NULL COMMENT 'รหัสผ่านแอดมิน'
+CREATE TABLE `tbl_admin`
+(
+  `a_username` varchar
+(100) NOT NULL COMMENT 'อีเมลแอดมิน',
+  `a_password` varchar
+(200) NOT NULL COMMENT 'รหัสผ่านแอดมิน'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_admin`
 --
 
-INSERT INTO `tbl_admin` (`a_username`, `a_password`) VALUES
+INSERT INTO `tbl_admin` (`
+a_username`,
+`a_password
+`) VALUES
 ('admin', 'admin@123');
 
 -- --------------------------------------------------------
@@ -45,20 +53,30 @@ INSERT INTO `tbl_admin` (`a_username`, `a_password`) VALUES
 -- Table structure for table `tbl_customers`
 --
 
-CREATE TABLE `tbl_customers` (
-  `c_id` int(11) NOT NULL COMMENT 'ไอดีลูกค้า',
-  `c_name` varchar(100) NOT NULL COMMENT 'ชื่อลูกค้า',
-  `c_tel` varchar(10) NOT NULL COMMENT 'เบอร์โทรลูกค้า',
-  `c_address` varchar(255) NOT NULL COMMENT 'ที่อยู่ลูกค้า',
-  `c_username` varchar(100) NOT NULL COMMENT 'ชื่อผู้ใช้การเข้าสู่ระบบลูกค้า',
-  `c_password` varchar(255) NOT NULL COMMENT 'รหัสการเข้าสู่ระบบลูกค้า'
+CREATE TABLE `tbl_customers`
+(
+  `c_id` int
+(11) NOT NULL COMMENT 'ไอดีลูกค้า',
+  `c_name` varchar
+(100) NOT NULL COMMENT 'ชื่อลูกค้า',
+  `c_tel` varchar
+(10) NOT NULL COMMENT 'เบอร์โทรลูกค้า',
+  `c_address` varchar
+(255) NOT NULL COMMENT 'ที่อยู่ลูกค้า',
+  `c_username` varchar
+(100) NOT NULL COMMENT 'ชื่อผู้ใช้การเข้าสู่ระบบลูกค้า',
+  `c_password` varchar
+(255) NOT NULL COMMENT 'รหัสการเข้าสู่ระบบลูกค้า'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_customers`
 --
 
-INSERT INTO `tbl_customers` (`c_id`, `c_name`, `c_tel`, `c_address`, `c_username`, `c_password`) VALUES
+INSERT INTO `tbl_customers` (`
+c_id`,
+`c_name
+`, `c_tel`, `c_address`, `c_username`, `c_password`) VALUES
 (1, 'พี่เจมส์ ตาโต', '0x0x0x0x0x', '...............', 'nathakrit', '12345678');
 
 -- --------------------------------------------------------
@@ -67,11 +85,15 @@ INSERT INTO `tbl_customers` (`c_id`, `c_name`, `c_tel`, `c_address`, `c_username
 -- Table structure for table `tbl_products`
 --
 
-CREATE TABLE `tbl_products` (
-  `p_id` int(11) NOT NULL COMMENT 'ไอดีสินค้า',
-  `p_name` varchar(100) NOT NULL COMMENT 'ชื่อสินค้า',
+CREATE TABLE `tbl_products`
+(
+  `p_id` int
+(11) NOT NULL COMMENT 'ไอดีสินค้า',
+  `p_name` varchar
+(100) NOT NULL COMMENT 'ชื่อสินค้า',
   `p_price` float NOT NULL COMMENT 'ราคาสินค้า',
-  `p_count` int(11) NOT NULL COMMENT 'จำนวนสินค้า'
+  `p_count` int
+(11) NOT NULL COMMENT 'จำนวนสินค้า'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -82,19 +104,22 @@ CREATE TABLE `tbl_products` (
 -- Indexes for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-  ADD PRIMARY KEY (`a_username`);
+ADD PRIMARY KEY
+(`a_username`);
 
 --
 -- Indexes for table `tbl_customers`
 --
 ALTER TABLE `tbl_customers`
-  ADD PRIMARY KEY (`c_id`);
+ADD PRIMARY KEY
+(`c_id`);
 
 --
 -- Indexes for table `tbl_products`
 --
 ALTER TABLE `tbl_products`
-  ADD PRIMARY KEY (`p_id`);
+ADD PRIMARY KEY
+(`p_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -104,8 +129,12 @@ ALTER TABLE `tbl_products`
 -- AUTO_INCREMENT for table `tbl_customers`
 --
 ALTER TABLE `tbl_customers`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ไอดีลูกค้า', AUTO_INCREMENT=2;
+  MODIFY `c_id` int
+(11) NOT NULL AUTO_INCREMENT COMMENT 'ไอดีลูกค้า', AUTO_INCREMENT=2;
 COMMIT;
+
+ALTER TABLE `tbl_products` CHANGE `p_id` `p_id` INT
+(11) NOT NULL AUTO_INCREMENT COMMENT 'ไอดีสินค้า';
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
