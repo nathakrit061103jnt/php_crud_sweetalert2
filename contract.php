6da600dd3e21de97d7c9af7f1545c5f548e3ccfc
james061103jnt <std60010912540@acc.msu.ac.th>
2020-12-01 00:23:58 +0700

    g

--- contract.php
+++ contract.php
@@ -42,7 +42,7 @@ if (isset($_SESSION['a_username'])) { ?>
                 <div class="col-md-7 mb-4">
 
                     <div class="view">
-                        <img src="https://scontent.fkkc3-1.fna.fbcdn.net/v/t1.0-9/119980752_337785307574954_1757152202764221432_o.jpg?_nc_cat=105&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeEdQ3uuMUpSNsKpHfjvpUDro1XX0a4IIyujVdfRrggjKx0oaTlnEEJRQqIYyg0KJMUxDMwjlPd-dN2hT7EGYqeo&_nc_ohc=0bT4uczorNcAX96yl_4&_nc_ht=scontent.fkkc3-1.fna&oh=6b8d05997d7e1dd2078a3062bc4a3886&oe=5FBB4911"
+                        <img src="https://scontent.fkkc3-1.fna.fbcdn.net/v/t1.0-9/120451642_340990133921138_1208408703837242985_o.jpg?_nc_cat=100&ccb=2&_nc_sid=174925&_nc_eui2=AeHvLI0r-WlEcRf11J6841T_LuM-Q2NlwcQu4z5DY2XBxDpvZuV-0ZTr_qnx0e-6-bkV15GCW4YnK-IZ5UTci8Ak&_nc_ohc=vHliceRcysgAX-vCw_J&_nc_ht=scontent.fkkc3-1.fna&oh=eaf2e99792558dbb21774bd790e44e40&oe=5FE96F79"
                             class="img-fluid" alt="smaple image">
                     </div>
 
--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if (isset($_SESSION['a_username'])) { ?>
 
     <br>
 
-    <div class="container my-5" ">
+    <div class="container my-5" >
         <div class=" row">
         <div class="col-md-10">
             <h3>
