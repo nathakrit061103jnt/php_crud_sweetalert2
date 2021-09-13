21249bf77affc0feed772b1e4de6aa9373224b65
nathakrit061103jnt <std60010912540@acc.msu.ac.th>
2021-09-13 15:21:41 +0700

    update connectDB to server heroku

--- config/connectDB.php
+++ config/connectDB.php
@@ -1,8 +1,17 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "php_crud_sweetalert2";
+
+// ## server ##
+$host = "us-cdbr-east-04.cleardb.com";
+$user = "b96f4b30055a5a";
+$password = "4d72ff7a";
+$database = "heroku_279a59c2954239d";
+
+
+// ## localhost ##
+// $host = "localhost";
+// $user = "root";
+// $password = "";
+// $database = "php_crud_sweetalert2";
 
 // เชื่อมต่อฐานข้อมูล
 $conn = mysqli_connect($host, $user, $password, $database);
